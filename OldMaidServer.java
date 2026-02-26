1dd92318fce7f1a6577bc3c4e571cb2eef44c18f
kmiyazaki-pixel <k_miyazaki@vital-area.com>
2026-02-26 10:29:06 +0900

    Update OldMaidServer.java

--- OldMaidServer.java
+++ OldMaidServer.java
@@ -12,40 +12,24 @@ public class OldMaidServer extends WebSocketServer {
     }
 
     @Override
-    @Override
-public void onOpen(WebSocket conn, ClientHandshake handshake) {
-    conns.add(conn);
-    System.out.println("新規接続: " + conn.getRemoteSocketAddress());
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        conns.add(conn);
+        System.out.println("New Connection: " + conn.getRemoteSocketAddress());
 
-    if (conns.size() == 2) {
-        // 1. 53枚のカード（1〜53）を準備
-        List<Integer> deck = new ArrayList<>();
-        for (int i = 1; i <= 53; i++) deck.add(i);
+        if (conns.size() == 2) {
+            // カード53枚を準備して配る
+            List<Integer> deck = new ArrayList<>();
+            for (int i = 1; i <= 53; i++) deck.add(i);
+            Collections.shuffle(deck);
 
-        // 2. シャッフル（混ぜる）
-        Collections.shuffle(deck);
+            List<Integer> hand1 = new ArrayList<>(deck.subList(0, 27));
+            List<Integer> hand2 = new ArrayList<>(deck.subList(27, 53));
 
-        // 3. 2人に分ける
-        List<Integer> hand1 = new ArrayList<>(deck.subList(0, 27)); // 27枚
-        List<Integer> hand2 = new ArrayList<>(deck.subList(27, 53)); // 26枚
-
-        // 4. それぞれに「あなたの手札」として送る
-        // フォーマット: HAND:1,5,12,44...
-        conns.get(0).send("HAND:" + listToString(hand1));
-        conns.get(1).send("HAND:" + listToString(hand2));
-        
-        broadcast("GAME_START:対戦開始！");
+            conns.get(0).send("HAND:" + listToString(hand1));
+            conns.get(1).send("HAND:" + listToString(hand2));
+            broadcast("GAME_START:対戦開始！");
+        }
     }
-}
-
-// リストをカンマ区切りの文字列にするための便利機能
-private String listToString(List<Integer> list) {
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < list.size(); i++) {
-        sb.append(list.get(i)).append(i == list.size() - 1 ? "" : ",");
-    }
-    return sb.toString();
-}
 
     @Override
     public void onMessage(WebSocket conn, String message) {
@@ -58,13 +42,27 @@ private String listToString(List<Integer> list) {
     }
 
     @Override
-    public void onError(WebSocket conn, Exception ex) { ex.printStackTrace(); }
+    public void onError(WebSocket conn, Exception ex) {
+        ex.printStackTrace();
+    }
 
     @Override
-    public void onStart() { System.out.println("Server Started"); }
+    public void onStart() {
+        System.out.println("Server Started");
+    }
 
     public void broadcast(String msg) {
-        for (WebSocket sock : conns) sock.send(msg);
+        for (WebSocket sock : conns) {
+            sock.send(msg);
+        }
+    }
+
+    private String listToString(List<Integer> list) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i)).append(i == list.size() - 1 ? "" : ",");
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {
